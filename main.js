74cb98bb257072cb77dd2bd25de240314f7471d5
Christopher Fujino <christopherfujino@gmail.com>
2016-11-22 00:19:12 -1000

    fill intensity

--- main.js
+++ main.js
@@ -13,6 +13,38 @@ $(function () {
     {
       intensity: 9,
       description: null
+    },
+    {
+      intensity: 8,
+      description: null
+    },
+    {
+      intensity: 7,
+      description: null
+    },
+    {
+      intensity: 6,
+      description: null
+    },
+    {
+      intensity: 5,
+      description: null
+    },
+    {
+      intensity: 4,
+      description: null
+    },
+    {
+      intensity: 3,
+      description: null
+    },
+    {
+      intensity: 2,
+      description: null
+    },
+    {
+      intensity: 1,
+      description: null
     }
   ];
 
